ce9a14791cb7822729d588aaf806baeaee5b02c1
Olivia Moldovan <oliviamoldovan56@gmail.com>
2026-02-16 23:15:41 +0200

    Create question.js with quiz questions
    Added a set of quiz questions related to Oliv's Closet.

---
+++ Oliv's Closet/question.js
@@ -0,0 +1,74 @@
+let questions = [ 
+    {
+        numb: 1,
+        question: "What year was Oliv's Closet created?",
+        answer: "b.2023",
+        options: [
+            "a.2020",
+            "b.2023",
+            "c.2021",
+            "d.2022"
+
+        ] ,
+    },
+    {
+        numb: 2,
+         question: "What are the new products?",
+        answer: "a.the t-shirts",
+        options: [
+            "a.the t-shirts",
+            "b.the skirts",
+            "c.the dresses",
+            "d.the socks"
+
+        ]
+    },
+    {
+        numb: 3,
+        question: "In which city is our closet located?",
+        answer: "d.Arad",
+        options: [
+
+            "a.Sibiu",
+            "b.Oradea",
+            "c.Brasov",
+            "d.Arad"
+        ],
+    },
+    {
+        numb: 4,
+        question: "Which is the predominant color in our closet?",
+        answer: "a.gray",
+        options: [
+            "a.gray",
+            "b.pink",
+            "c.red",
+            "d.green"
+
+        ],
+    },
+    {
+        numb: 5,
+        question: "Who is the girl from the website logo?",
+        answer: "c.Olivia",
+        options: [
+            "a.Andreea",
+            "b.Miruna",
+            "c.Olivia",
+            "d.Andrei"
+
+        ],
+    },
+    {
+        numb:6,
+        question: "How many times a month do we have promotions?",
+        answer: "a.two times",
+        options: [
+             "a.two times",
+             "b.three times",
+             "c.one time",
+             "d.five times"
+        ]
+    }
+];
+
